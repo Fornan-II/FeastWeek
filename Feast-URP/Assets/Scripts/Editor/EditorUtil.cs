cec9c03ff25121400eee31e517d9146440f7045b
Maxen McCoy <maxcoy97@gmail.com>
2020-11-29 09:56:45 -0500

    Only the end of the level is left (also did particles)

--- Feast-URP/Assets/Scripts/Editor/EditorUtil.cs
+++ Feast-URP/Assets/Scripts/Editor/EditorUtil.cs
@@ -6,6 +6,8 @@ public static class EditorUtil
 {
     [MenuItem("Tools/Open Scene/Level")]
     public static void OpenScene_Level() => EditorSceneManager.OpenScene("Assets/Scenes/Level.unity", OpenSceneMode.Single);
+    [MenuItem("Tools/Open Scene/SampleScene")]
+    public static void OpenScene_SampleScene() => EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity", OpenSceneMode.Single);
 
     [MenuItem("Tools/Toggle Render Features")]
     public static void ToggleFog()

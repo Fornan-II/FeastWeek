97614752c5a8c99f10895b1f2788dd580e22fbfd
Maxen McCoy <maxcoy97@gmail.com>
2020-11-30 10:52:56 -0500

    Finished Geometry (Outside)

---
+++ Feast-URP/Assets/Scripts/WaterPlaneBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPlaneBehavior : MonoBehaviour
+{
+    private float _yPosition;
+
+    private void Start() => _yPosition = transform.position.y;
+
+    private void Update()
+    {
+        if (!MainCamera.Instance) return;
+
+        Vector3 position = MainCamera.Instance.transform.position;
+        position.y = _yPosition;
+        transform.position = position;
+    }
+}
